1036d9af5abdd5accc32c89cec0e7cafec8c894a
Temuri Takalandze <takalandzet@gmail.com>
2021-09-05 15:45:46 +0400

    refactor: move application logic to the separated package

---
+++ vaccination/core/__init__.py
@@ -0,0 +1,10 @@
+"""
+Core package.
+
+This file is part of the vaccination.py.
+
+(c) 2021 Temuri Takalandze <me@abgeo.dev>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+"""
